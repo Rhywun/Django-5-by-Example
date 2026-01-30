aa6295845fcd2171ad3fbff06e62ac132d37456c
Patrick Huffer <patrick.huffer@proton.me>
2026-01-30 07:16:19 -0500

    Mid ch02 - Adding pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 
@@ -5,7 +6,12 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.publishedObjects.all()
+    post_list = Post.publishedObjects.all()
+
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.get_page(page_number)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
